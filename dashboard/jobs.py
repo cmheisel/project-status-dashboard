4c36a07f0ca8d49412442d6d5b5e6acb1c60cbbd
Chris Heisel <chris@heisel.org>
2016-07-09 17:00:40 -0400

    Gather and display the week ago, if available

--- dashboard/jobs.py
+++ dashboard/jobs.py
@@ -1,5 +1,7 @@
 import datetime
 
+from dateutil.relativedelta import relativedelta
+
 from django.conf import settings
 from django.core.cache import cache
 from django.utils.timezone import get_default_timezone
@@ -26,6 +28,10 @@ def generate_dashboard():
                 )
                 summaries.store(p)
                 row.xtras['jira_summary'] = p
+
+                week_ago = p.created_on - relativedelta(days=7)
+                week_ago_summary = summaries.for_date(filter_id=p.filter_id, date=week_ago)
+                row.xtras['week_ago_summary'] = week_ago_summary
     cache.set('dashboard_data', data, None)
     cache.set('dashboard_data_updated', datetime.datetime.now(get_default_timezone()), None)
     return True
