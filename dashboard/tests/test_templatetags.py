c14db8d81e5efd6f345a4e5acd77699bb9018d7f
Chris Heisel <chris@heisel.org>
2016-07-21 10:04:02 -0400

    Add a convieient fixture

--- dashboard/tests/test_templatetags.py
+++ dashboard/tests/test_templatetags.py
@@ -10,9 +10,30 @@ def percentage_filter():
     return percentage
 
 
+@pytest.fixture
+def render_template():
+    """Provide function for rendering a template string."""
+    from django.template import Template, Context
+
+    def _render_template(template_string, context={}):
+        return Template(template_string).render(Context(context))
+
+    return _render_template
+
+
 def test_percentage(percentage_filter):
     """Floats come back with 1 place of precision."""
 
     expected = "22.2%"
     actual = percentage_filter(.2221)
+    assert expected == actual
+
+
+def test_render_template(render_template):
+    """Ensure our fixture does what it says."""
+    expected = "Name the ship: Rocinante"
+
+    template = r"""Name the ship: {{ ship_name }}"""
+    actual = render_template(template, dict(ship_name="Rocinante"))
+
     assert expected == actual
